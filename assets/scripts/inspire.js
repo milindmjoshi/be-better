d9ede5b682ac456d5cf058470a0234c103c736b9
MaslowMae <bmm10025@gmail.com>
2024-01-31 20:03:22 -0700

    updated inspire html buttons and javascript, need to travers the dom and make button functions

--- assets/scripts/inspire.js
+++ assets/scripts/inspire.js
@@ -1,5 +1,15 @@
 
-fetch('http://www.boredapi.com/api/activity?participants=1&participants=1')
-.then(function(res) {return res.json()})
-.then(function(data){console.log(data)})
 
+inspireActivityRun = fetch('http://www.boredapi.com/api/activity?participants=1&participants=1')
+
+    .then(function (res) { return res.json() })
+    .then(function (data) { console.log(data) })
+    document.getElementById("inspireNext").data.activity
+    // trying to get the function to output to the DOM for visible activity to show to user
+
+var inspireNextBtn=
+
+    document.getElementById("inspireNext").addEventListener("click", inspireActivityRun.activity) 
+       
+
+    
