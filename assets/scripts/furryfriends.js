ba6d1415d3a8decbc325e507fdb69b1f73b747bf
Milman <milindmjoshi@live.com>
2024-02-01 16:44:54 -0500

    Add furry friends panel and clean up jokes

---
+++ assets/scripts/furryfriends.js
@@ -0,0 +1,33 @@
+var furrytile = document.querySelector("#furryImage");
+
+var funnySayings = 
+  [
+    "Don't hate me because I am beautiful",
+    "Today is a good day to have some fun",
+    "Hello",
+    "More beer please",
+    "Be the best you can be",
+    "You too can be great",
+    "Its a tough job",
+    "Dont you wish your girlfriend was hot like me",
+    "I'm amazing"
+  ]
+
+function randomSaying(){
+    return funnySayings[Math.floor(Math.random()*funnySayings.length)];
+  }
+
+//fetch('https://cataas.com/cat/says/hello')
+fetch("https://cataas.com/cat/says/" + randomSaying() + "?fontSize=30&fontColor=yellow")
+  .then(function (response) {
+    return response.blob();
+  })
+  .then(function (blob){
+    var imageURL = URL.createObjectURL(blob);
+    console.log(imageURL);
+    furrytile.setAttribute("src",imageURL);
+  }).catch((e) => console.log(e));
+
+
+
+  
