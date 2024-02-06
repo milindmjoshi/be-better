ce915c58b7a46accb1eb8f01a81cb1ffd6748cc9
Jonathanfajardo <jonnyfajardo18@gmail.com>
2024-02-05 20:00:21 -0500

    switching my api to a database that pulls strings of data i added which are simple workout suggestions

--- assets/scripts/fitness.js
+++ assets/scripts/fitness.js
@@ -1,51 +1,39 @@
-const settings = {
-    async: true,
-    crossDomain: true,
-    url: 'https://youtube-trending.p.rapidapi.com/trending?country=US&type=Fitness',
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '18d8053150mshe91e7ced9dede79p12feb9jsn91c1435a3b0b',
-		'X-RapidAPI-Host': 'youtube-trending.p.rapidapi.com'
-    }
-};
-
-function displayVideos(videos) {
-    const videoContainer = $('#video-container');
-    //to clear previous videos
-    videoContainer.empty();
-
-        const videoId = video.id;
-        const videoTitle = video.title;
-
-        //this should embed the youtube video using the video ID not sure whats wrong
-        videoContainer.append(`
-        <div>
-            <iframe width="300" height="200" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
-            <p>${videoTitle}</p>
-        </div>
-    `);
-    }
-//display a loading message while they wait on the video to load
-function displayLoadingMessage() {
-    const videoContainer = $('video-container');
-    videoContainer.empty();
-
-    videoContainer.append('<p>Loading...</p>');
-}
-
-displayLoadingMessage();
-//make the request
-$.ajax(settings)
-    .done(function(response){
-        console.log(response);
-        const videos = response.items;
-
-        if (videos && videos.length > 0) {
-        displayVideo(videos[0]);
-        } else {
-        $('#video-container').html('<p>No videos available.</p>');
-        }
-    })
+// Import Firebase SDK
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js';
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js';
 
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCrtLPit-X3-4yLBhTrUvJ3dkdwnZCRafI",
+    authDomain: "be-better-workouts.firebaseapp.com",
+    projectId: "be-better-workouts",
+    storageBucket: "be-better-workouts.appspot.com",
+    messagingSenderId: "440633074186",
+    appId: "1:440633074186:web:a3a5c354d45d35e1f075f3",
+    measurementId: "G-CTVBE3V19S"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const db = getFirestore(app);
+
+  //add function to fetch workout suggestions and show them
+  function fetchWorkoutSuggestions() {
+    const workoutList = document.getElementById('workoutList');
+
+    workoutList.innerHTML = '';
+
+    db.collection('workouts').get().then((querySnapshot) => {
+        querySnapshot.forEach((doc)=> {
+            const workout = doc.data().name;
+            const listItem = document.createElement('li');
+            listItem.textContent = workout;
+            workoutList.appendChild(listItem);
+        });
+    });
+  }
+
+  document.addEventListener('DOMContentLoaded',fetchWorkoutSuggestions);
 
