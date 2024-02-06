2c50aa5a5ecc5ee037d2bdefe6381975bdf77b4a
Nymph <trismageefl@gmail.com>
2024-02-05 19:11:11 -0500

    Cleaned up html and css & added a html page to furry friends

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,5 +6,17 @@ inspireLink.addEventListener('click',function() {
     window.location = "inspire.html";
 });
 
-console.log(inspireLink)
+console.log(inspireLink);
+
+
+
+var inspireLink = document.querySelector(".furry-link");
+
+inspireLink.addEventListener('click',function() {
+    window.location = "furry.html";
+});
+
+console.log(furryLink)
+
+
 
